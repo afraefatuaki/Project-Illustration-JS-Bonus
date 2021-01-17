2a7472740365b746690a32bb67ee58fe7334d072
afraefatuaki <afrae@elfatuaki.com>
2021-01-17 20:18:14 +0100

    Project Illustration + JS Bonus

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+
+
+function darkMode() {
+    document.body.classList.toggle("darkMode")
+    document.body.style.backgroundColor = "#000000"
+    document.getElementById("Text").style.color = "#FFFF"
+    document.getElementById("Text1").style.color = "#FFFF"
+    document.getElementById("Text2").style.color = "#FFFF"
+    document.getElementById("Text3").style.color = "#FFFF"
+    document.getElementById("Text4").style.color = "#FFFF"
+    document.getElementById("Text5").style.color = "#FFFF"
+    document.getElementById("Text6").style.color = "#FFFF"
+    document.getElementById("Text7").style.color = "#FFFF"
+    document.getElementById("Text8").style.color = "#FFFF"
+    document.getElementById("Text9").style.color = "#FFFF"
+    document.getElementById("Text10").style.color = "#FFFF"
+    document.getElementById("Text11").style.color = "#FFFF"
+    document.getElementById("Text12").style.color = "#FFFF"
+    document.getElementById("Text13").style.color = "#FFFF"
+    document.getElementById("Text14").style.color = "#FFFF"
+    document.getElementById("Text15").style.color = "#FFFF"
+    document.getElementById("Text16").style.color = "#FFFF"
+    document.getElementById("Text17").style.color = "#FFFF"
+    document.getElementById("anchor").style.color = "#FFFF"
+    document.getElementById("anchor1").style.color = "#FFFF"
+    document.getElementById("anchor2").style.color = "#FFFF"
+    document.getElementById("anchor3").style.color = "#FFFF"
+    document.getElementById("anchor3").style.backgroundColor = "#091219"
+    document.getElementById("anchor4").style.color = "#091219"
+    document.getElementById("anchor4").style.backgroundColor = "#FFFF"
+    document.getElementById("anchor5").style.color = "#FFFF"
+    document.getElementById("illustration").src = 'assets/img/darkmode-img4.png'
+    document.getElementById("illustration1").src = 'assets/img/darkmode-img5.png'
+    document.getElementById("illustration2").src = 'assets/img/darkmode-img6.png'
+    document.getElementById("memberShip").style.backgroundColor = "#091219"
+    document.getElementById("gray").style.color = "gray"
+    document.getElementById("gray1").style.color = "gray"
+    document.getElementById("gray2").style.color = "gray"
+    document.getElementById("gray3").style.color = "gray"
+    document.getElementById("gray4").style.color = "gray"
+}
